8ef9360ec3f39b72b51c4b2bb4a6f91da9746530
Taiwo <ajayitaiwotunta@gmail.com>
2022-12-28 22:15:40 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
 const navBar = document.querySelector(".navbar");
 const toggler = document.querySelector(".toggler");
-const navBarNav = document.querySelector(".navbar ul");
+const navBarNav = document.querySelector(".navbar-collapse");
 
+// Fade in effects
 AOS.init({
   duration: 1000,
 });
 
-
-$("nav a").on("click", scroll);
-$("#banner a").on("click", scroll);
-
-function scroll(e) {
-   e.preventDefault();
-
-   const href = $(this).attr("href");
-
+// Smooth scrolling
+$("nav a").on("click", function (e) {
+  e.preventDefault();
+  const href = $(this).attr("href");
   $("html, body").animate({ scrollTop: $(href).offset().top }, 1000);
-   navBar.classList.remove("show");
-}
-window.addEventListener("scroll", function (e) {
-  if (Number(e.target.body.scrollTop) > 0) {
-    navBar.classList.add("show-navbar-bg");
-  } else navBar.classList.remove("show-navbar-bg");
-  this.document.getElementById('navigation').classList.remove('show')
-
 });
 
-toggler.addEventListener("click", function (e) {
-  toggler.classList.toggle("turn-toggler");
-  if (Number(document.body.scrollTop) < 100) {
-    navBar.classList.add("show-navbar-bg");
-  }
-});
 
-if (Number(document.body.scrollTop) >= 100) {
+// toggler
+toggler.onclick = function () {
+  toggler.classList.toggle('turn-toggler')
   navBar.classList.add("show-navbar-bg");
-} else navBar.classList.remove("show-navbar-bg");
+}
+
+
+// Onscroll effect
+document.body.onscroll = function () {
+  toggler.classList.remove('turn-toggler')
+  navBarNav.classList.remove('show')
+  if (document.body.scrollTop == 0) {
+    navBar.classList.remove("show-navbar-bg");
+  } else navBar.classList.add("show-navbar-bg");
+}
+
